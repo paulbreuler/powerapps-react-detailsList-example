ecadba84e7e5175d339b37448b6fde4b0ba6ac26
Paul Breuler <4356863+paulbreuler@users.noreply.github.com>
2021-02-02 23:27:58 -0500

    Fix bug and allow test for query ending with s or ies

--- src/App.tsx
+++ src/App.tsx
@@ -49,9 +49,14 @@ export default class App extends React.Component<{}, IAppState> {
     let responseJson = await response.json();
 
     // To be more generic you would need to be able to accept interfaces that can then be deconstructed
-    let idKey = `${this.state.query.substring(0, this.state.query.length - 1)}id`
-    var mappedResponse = responseJson.value.map((item: any) => { const container = { [idKey]: item.accountid, "name": item.name }; return container; })
-
+    let idKey = "";
+    if(this.state.query.endsWith("ies")){
+      idKey = `${this.state.query.substring(0, this.state.query.length - 3)}yid`
+    } else{
+      idKey = `${this.state.query.substring(0, this.state.query.length - 1)}id`
+    }
+    var mappedResponse = responseJson.value.map((item: any) => { const container = { [idKey]: item[idKey], "name": item.name }; return container; })
+    
     let columns: any = [];
     Object.keys(mappedResponse[0]).forEach((column: any) => {
       debugger;
@@ -85,7 +90,7 @@ export default class App extends React.Component<{}, IAppState> {
     return (
       <Fabric>
         <TextField value={this.state.query} label="Api Query" onChange={this.onChangeText} />
-        <DefaultButton text="Submit Request" onClick={() => this.apiRequest("accounts")} allowDisabledFocus />
+        <DefaultButton text="Submit Request" onClick={() => this.apiRequest(this.state.query)} allowDisabledFocus />
         { this.state.listCollection.length > 0 && this.state.listColumns.length > 0 ? <DetailsList columns={this.state.listColumns} items={this.state.listCollection} /> : null}
       </Fabric>
     )
